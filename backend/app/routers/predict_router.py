8810dd1a6bca984e1843401febb9df1872afc7c9
ThanakornKaingam <thanakornkaingam@gmail.com>
2026-09-10 03:52:05 +0700

    Update predict_router.py

--- backend/app/routers/predict_router.py
+++ backend/app/routers/predict_router.py
@@ -75,7 +75,7 @@ VEGETABLE_INFO = {
 
 router = APIRouter(prefix="/predict", tags=["Prediction"])
 
-CONFIDENCE_THRESHOLD = 0.37
+CONFIDENCE_THRESHOLD = 0.20
 
 
 # =============================================================
